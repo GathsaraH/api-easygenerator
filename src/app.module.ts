5eacd767d15eddfb2a0b67ed940793e88ac1583b
Gathsara Umesh <66924429+GathsaraH@users.noreply.github.com>
2024-07-07 11:53:37 +0530

    feat: setup database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database-config/config/database.config';
+import appConfig from './config/app-config/app.config';
+import { DatabaseModule } from './config/database-config/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
